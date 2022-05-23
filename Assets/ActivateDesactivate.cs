691e0990ee1901925dc89476989f00bf0ba8e6e0
KELMAN NICOLAS <47297850@ort>
2022-05-23 09:27:15 -0300

    Initial commit

---
+++ Assets/ActivateDesactivate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateDesactivate : MonoBehaviour
+{
+    public GameObject inputField;
+    public bool wasPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wasPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateandDesactive()
+    {
+        if (!wasPressed)
+        {
+            inputField.SetActive(false);
+            wasPressed = true;
+        }
+        else
+        {
+            inputField.SetActive(true);
+            wasPressed = false;
+        }
+    }
+}
